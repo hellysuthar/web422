7c19602e4fc721a8dc4c887a8868b43d704e13d9
Helly <hbsuthar@myseneca.ca>
2023-08-04 20:39:11 -0400

    update

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 
 const HTTP_PORT = process.env.PORT || 80;
